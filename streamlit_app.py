150ff7019c46c0d7c2ffd89dd1d3d48e82987ba6
ddat03 <diego.aleman@yachaytech.edu.ec>
2025-06-27 21:03:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 import pickle
+from sklearn.preprocessing import LabelEncoder
 import warnings
 warnings.filterwarnings('ignore')
 
